c4414d45d6e8805bb879f279dd1b6f091d7f6d50
Midzuki <ucdmzk@icloud.com>
2018-09-22 11:35:15 +0900

    -

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,11 +3,12 @@ module Lib
     ) where
 
 import           Codec.Midi
-
+import Data.Ratio
+import Data.List
 
 track0 = [(0,  NoteOn 0 60 80),
-          (1,  NoteOff 0 60 0),
           (0,  NoteOn 0 64 60),
+          (1,  NoteOff 0 60 0),
           (12, NoteOff 0 64 0),
           (0,  NoteOn 0 61 60),
           (12,  NoteOff 0 61 60),
@@ -21,8 +22,22 @@ myMidi = Midi { fileType = MultiTrack,
                 timeDiv  = TicksPerBeat 240,
                 tracks   = [track0, track1] }
 
+data Sound = Sound Int
 
-data Rhythm = Unit | Rhythm Bool [Rhythm]
+data Rhythm = Unit Sound | Rhythm Int Bool [Rhythm]
 -- True -> 直列
 -- False -> 並列
 
+{-
+--Rhythmの数値化
+convertRhythm :: Rhythm -> (Int, [(Ratio Int, Sound)])
+convertRhythm (Unit s) = (1, [(1 % 1, s)])
+convertRhythm (Rhythm n False rs) = (n, concat $ snd . convertRhythm <$> rs)
+convertRhythm (Rhythm n True rs) =
+  let
+    xs = convertRhythm <$> rs
+    ys = f <$> xs
+  where
+    f (n_, )
+  (n, concat $ snd . convertRhythm <$> rs)
+-}
